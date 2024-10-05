3eeed22a34262a5c9c8adf01a130f28191670990
eyoaab <eyobtariku48@gmail.com>
2024-10-06 01:29:59 +0300

    added the api end points

---
+++ abissinia_mobile_project/lib/core/constants/Urls.dart
@@ -0,0 +1,22 @@
+class Urls {
+  static String servicesUrl() => "https://backend.abyssiniasoftware.com/api/service";
+  static String servicesUrlById(String id) => 'https://backend.abyssiniasoftware.com/api/service/$id';
+
+  static String userLogin() => "https://backend.abyssiniasoftware.com/api/user/login";
+  static String userSignup() => 'https://backend.abyssiniasoftware.com/api/user/signup';
+
+  static String faqUrl() => "https://backend.abyssiniasoftware.com/api/faq";
+  static String faqUrlById(String id) => 'https://backend.abyssiniasoftware.com/api/faq/$id';
+
+  static String sliderUrl() => "https://backend.abyssiniasoftware.com/api/slider";
+  static String sliderUrlById(String id) => 'https://backend.abyssiniasoftware.com/api/slider/$id';
+
+  static String testimonyUrl() => "https://backend.abyssiniasoftware.com/api/testimony";
+  static String testimonyById(String id) => 'https://backend.abyssiniasoftware.com/api/testimony/$id';
+
+  static String productUrl() => "https://backend.abyssiniasoftware.com/api/product";
+  static String productById(String id) =>  'https://backend.abyssiniasoftware.com/api/product/$id';
+
+  static String blogUrl() => "https://backend.abyssiniasoftware.com/api/blogs";
+  static String blogUrlById(String id) => 'https://backend.abyssiniasoftware.com/api/blogs/$id';
+}
